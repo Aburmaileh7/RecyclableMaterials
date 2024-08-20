126ae18f1147531af5a59bac301a3ddf942c9be3
Abuali <Aburmaileh7@gmail.com>
2024-08-20 17:43:58 +0300

    0

---
+++ RecyclableMaterials/Controllers/BidController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecyclableMaterials.Data;
+using RecyclableMaterials.Models;
+
+namespace RecyclableMaterials.Controllers
+{
+    public class BidController : Controller
+    {
+        private RDBContext _dBContext;
+        public BidController(RDBContext dBContext)
+        {
+            this._dBContext=dBContext;
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(int ProductId, decimal amount)
+        {
+            var product = await _dBContext.products.FindAsync(ProductId);
+            if (product == null || amount <= 0)
+            {
+                return BadRequest("Invalid bid.");
+            }
+
+            var highestBid = _dBContext.Bids
+                .Where(b => b.ProductId == ProductId)
+                .OrderByDescending(b => b.Amount)
+                .FirstOrDefault();
+
+            if (highestBid != null && amount <= highestBid.Amount)
+            {
+                return BadRequest("Your bid must be higher than the current highest bid.");
+            }
+
+            var bid = new BidModel
+            {
+                ProductId = ProductId,
+                Amount = amount,
+                //UserId = Userid
+                CreatedAt = DateTime.Now
+            };
+
+            _dBContext.Add(bid);
+            await _dBContext.SaveChangesAsync();
+            return RedirectToAction("Details", "Material", new { id = ProductId });
+        }
+    }
+}
---
+++ RecyclableMaterials/Models/BidModel.cs
@@ -0,0 +1,18 @@
+﻿using RecyclableMaterials.ViewModels;
+
+namespace RecyclableMaterials.Models
+{
+    public class BidModel
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        public ProductModel product { get; set; }
+
+        public int UserId { get; set; }
+        public UserViewModel User { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
